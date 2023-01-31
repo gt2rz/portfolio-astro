bc7c563c5259231c83b2c55143a08224b41d5cf2
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-01-31 18:45:09 -0500

    update

---
+++ src/services/workExperiense.js
@@ -0,0 +1,64 @@
+export const work_experiences = [
+    {
+        title: "Software developer",
+        business: {
+            name: "Laika Mascotas",
+            location: "Bogota, Colombia"
+        },
+        dates: {
+            start: "sept. 2020",
+            final: ""
+        },
+        description: "Development of applications in laravel and vuejs for internal users. Development of stored procedures in mysql. Development of commands for replicas of promotions and different products.",
+        tags: [
+            "PHP",
+            "Laravel",
+            "Vuejs",
+            "Mysql",
+            "Stored procedured",
+            "Jobs",
+            "Netsuite",
+            "Ecommerse"
+        ]
+    },
+    {
+        title: "Software developer",
+        business: {
+            name: "Freelance",
+            location: "Remote"
+        },
+        dates: {
+            start: "apr. 2019",
+            final: "aug. 2020"
+        },
+        description: "Software development aimed at providing solutions to different needs in different companies.",
+        tags: [
+            "PHP",
+            "Laravel",
+            "Mysql",
+            "Postgres",
+            "Linux",
+            "Javascript",
+            "Excel"
+        ]
+    },
+    {
+        title: "Software Development Consultant",
+        business: {
+            name: "ProcessimLabs",
+            location: "Remote"
+        },
+        dates: {
+            start: "mar. 2019",
+            final: "abr. 2019"
+        },
+        description: "Development of a web application (Frontend - ReactJs), aimed at managing the purchase of the different products of the company. Integration with Auth0 and Stripe, among other features.",
+        tags: [
+            "React",
+            "Auth0",
+            "Stripe",
+            "Frontend",
+            "Others"
+         ]
+    },
+]
